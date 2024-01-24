55955bd0a9840f8df1c9db62ca69f85042ca9dea
Michal Jan Warecki <mwarecki1@gmail.com>
2024-01-24 23:02:59 +0100

    Frontend setup

--- veke-frontend/vite.config.ts
+++ veke-frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
+  build: {
+    outDir: '../public/assets',
+    rollupOptions: {
+      input: {
+        'script': 'src/main.ts',
+        'styles': 'src/index.css',
+      },
+      output: {
+        assetFileNames: "[name].[ext]"
+      }
+    },
+  },
 })
