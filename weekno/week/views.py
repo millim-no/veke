1b2caf86eee9b9fcee5a7a618bde196636d3583e
Michal Jan Warecki <mwarecki1@gmail.com>
2024-01-30 13:29:04 +0100

    Data in the calendar card.

--- weekno/week/views.py
+++ weekno/week/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
+import time
+from datetime import date, timedelta
 # Create your views here.
+
+
+MONTHS = ["januar", "februar", "mars", "april", "mai", "juni",
+          "juli", "august", "september", "oktober", "november", "desember"]
+WEEKDAYS = ["måndag", "tysdag", "onsdag", "torsdag", "fredag",
+            "laurdag", "sundag"]
+
 
 class DayView(TemplateView):
     template_name = "today.html"
 
     def get_context_data(self, **kwargs):
+        today = date(kwargs['year'], kwargs['month'], kwargs['day']) \
+            if (len(kwargs) > 0) else date.today()
+
         context = super().get_context_data(**kwargs)
-        print(dir(self.request))
-        #context["latest_articles"] = Article.objects.all()[:5]
+        context["week"] = today.isocalendar()[1]
+        context["ordinal_day"] = int(today.strftime("%j"))
+        context["month"] = MONTHS[today.month - 1]
+        context["day"] = today.day
+        context["weekday"] = WEEKDAYS[today.isoweekday() - 1]
+        context["full_slots"] = [x for x in range(today.isoweekday())]
+        context["empty_slots"] = [x for x in range(7 - today.isoweekday())]
         return context
