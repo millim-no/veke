e3342e7bd6e22c7c8349e9b0f14bb35baf68fd33
Michal Jan Warecki <mwarecki1@gmail.com>
2024-01-30 13:29:26 +0100

    Pick date from the URL

--- weekno/week/urls.py
+++ weekno/week/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import DayView
 
 urlpatterns = [
-    path("", DayView.as_view())
+    path("", DayView.as_view(), name="today"),
+    path("<int:year>/<int:month>/<int:day>", DayView.as_view(), name="by_day"),
 ]
